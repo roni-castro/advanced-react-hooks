22529543b6200ea91276c3578e1297352aec9c78
Roni <roni.c.castro@gmail.com>
2020-10-25 18:17:22 -0300

    Allow action to be a function or an object

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,16 +4,20 @@
 import React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, changeCount] = React.useReducer(countReducer, {
+  const [state, setState] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
 
   function countReducer(state, action) {
-    return {count: action}
+    if (typeof action === 'function') {
+      return action(state)
+    }
+    return action
   }
 
-  const increment = () => changeCount(count + step)
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
